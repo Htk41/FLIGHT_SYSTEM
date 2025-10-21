056332cd5f815a341b11fb25b48935774b49722f
Hoang Kiet <hoangkiet4104@gmail.com>
2025-10-21 09:52:38 +0000

    thêm xác thực người dùng (Đăng ký, Đăng nhập)

--- flight-system-api/app.js
+++ flight-system-api/app.js
@@ -18,4 +18,35 @@ app.use(compression());
 //MongoDB Connection
 mongoose.set("useNewUrlParser", true);
 mongoose.set("useFindAndModify", false);
-mongoose.set("useCreateIndex", true);
+mongoose.set("useCreateIndex", true);
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("Connected to database");
+  })
+  .catch(err => {
+    console.log("Error: ", err.message);
+  });
+
+//MiddleWares
+app.use(express.static("upload"));
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(cookieParser());
+//Routes
+app.use("/api/auth", AuthRouter);
+app.use("/api/users", userRouter);
+app.use("/api/flights", flightRouter);
+app.use("/api/blogs", blogsRouter);
+//Unauthorized Handler
+app.use(function(err, req, res, next) {
+  if (err.name === "UnauthorizedError") {
+    res
+      .status(401)
+      .json({ error: "You are not Authorized to perform this Action" });
+  }
+});
+
+server.listen(PORT, () => {
+  console.log(`Server Running on PORT: ${PORT}`);
+});
